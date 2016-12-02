c9e565a914beb5d64d3a60f4f4b40e0980c86362
Landon Holmes <landon.holmes@gmail.com>
2016-12-02 11:39:48 -0600

    Finally fixed puzzle 2 for day 1. Will now find first repeated coord that we run across

--- src/Day1Puzzle2.py
+++ src/Day1Puzzle2.py
@@ -1,26 +1,18 @@
-# instruction input but this time any time I overlap where I have gone before, that is the solution
-directions = ['R5', 'L2', 'L1', 'R1', 'R3', 'R3', 'L3', 'R3', 'R4', 'L2', 'R4', 'L4', 'R4', 'R3', 'L2', 'L1', 'L1', 'R2', 'R4', 'R4', 'L4', 'R3', 'L2', 'R1', 'L4', 'R1', 'R3', 'L5', 'L4', 'L5', 'R3', 'L3', 'L1', 'L1', 'R4', 'R2', 'R2', 'L1', 'L4', 'R191', 'R5', 'L2', 'R46', 'R3', 'L1', 'R74', 'L2', 'R2', 'R187', 'R3', 'R4', 'R1', 'L4', 'L4', 'L2', 'R4', 'L5', 'R4', 'R3', 'L2', 'L1', 'R3', 'R3', 'R3', 'R1', 'R1', 'L4', 'R4', 'R1', 'R5', 'R2', 'R1', 'R3', 'L4', 'L2', 'L2', 'R1', 'L3', 'R1', 'R3', 'L5', 'L3', 'R5', 'R3', 'R4', 'L1', 'R3', 'R2', 'R1', 'R2', 'L4', 'L1', 'L1', 'R3', 'L3', 'R4', 'L2', 'L4', 'L5', 'L5', 'L4', 'R2', 'R5', 'L4', 'R4', 'L2', 'R3', 'L4', 'L3', 'L5', 'R5', 'L4', 'L2', 'R3', 'R5', 'R5', 'L1', 'L4', 'R3', 'L1', 'R2', 'L5', 'L1', 'R4', 'L1', 'R5', 'R1', 'L4', 'L4', 'L4', 'R4', 'R3', 'L5', 'R1', 'L3', 'R4', 'R3', 'L2', 'L1', 'R1', 'R2', 'R2', 'R2', 'L1', 'L1', 'L2', 'L5', 'L3', 'L1']
-current_direction = 1
-distance_traveled_in_direction = {
-    1: 0,  # north
-    2: 0,  # east
-    3: 0,  # south
-    4: 0  # west
+directions = 'R5, L2, L1, R1, R3, R3, L3, R3, R4, L2, R4, L4, R4, R3, L2, L1, L1, R2, R4, R4, L4, R3, L2, R1, L4, R1, R3, L5, L4, L5, R3, L3, L1, L1, R4, R2, R2, L1, L4, R191, R5, L2, R46, R3, L1, R74, L2, R2, R187, R3, R4, R1, L4, L4, L2, R4, L5, R4, R3, L2, L1, R3, R3, R3, R1, R1, L4, R4, R1, R5, R2, R1, R3, L4, L2, L2, R1, L3, R1, R3, L5, L3, R5, R3, R4, L1, R3, R2, R1, R2, L4, L1, L1, R3, L3, R4, L2, L4, L5, L5, L4, R2, R5, L4, R4, L2, R3, L4, L3, L5, R5, L4, L2, R3, R5, R5, L1, L4, R3, L1, R2, L5, L1, R4, L1, R5, R1, L4, L4, L4, R4, R3, L5, R1, L3, R4, R3, L2, L1, R1, R2, R2, R2, L1, L1, L2, L5, L3, L1'
+# directions = 'R8, R4, R4, R8'
+current_direction = 1  # 1: North, 2: East, 3: South, 4: West
+grid_locations_current_coordinates = {
+    'x': 0,
+    'y': 0
 }
-useful_distance_traveled = 0
-
-grid_locations_size = 1000
-grid_locations = [[0 for x in range(grid_locations_size)] for y in range(grid_locations_size)]  # [1000,1000] grid of locations we have either visited (1) or not (0)
-grid_locations_coordinates = {
-    'x': grid_locations_size / 2,
-    'y': grid_locations_size / 2
-}
-bunny_hq_found = 0
+grid_locations_coordinates_visited = [{'x': 0, 'y': 0}]
 bunny_hq_coordinates = {
     'x': 0,
     'y': 0
 }
+found_hq = False
 
+# function declaration
 
 def change_direction(turn):
     global current_direction
@@ -37,43 +29,57 @@ def change_direction(turn):
     elif current_direction == 0:
         current_direction = 4
 
+    return
+
+
+def check_if_already_visited(coordinates_to_check):
+    global grid_locations_coordinates_visited
+    for coordinates in grid_locations_coordinates_visited:
+        if coordinates['x'] == coordinates_to_check['x'] and coordinates['y'] == coordinates_to_check['y']:
+            return True
+
+    return False
+
 
 # computes the movement of santa and marks nodes as visited or not
-def compute_movement(instruction_distance):
+def compute_movement(distance_to_move):
     global current_direction
-    global grid_locations
-    global grid_locations_coordinates
-    global bunny_hq_found
+    global grid_locations_current_coordinates
+    global grid_locations_coordinates_visited
     global bunny_hq_coordinates
+    global found_hq
 
-    while instruction_distance != 0:
-        if grid_locations[grid_locations_coordinates['x'], grid_locations_coordinates['y']] == 1:
-            bunny_hq_found = 1
-            bunny_hq_coordinates = {grid_locations_coordinates['x'], grid_locations_coordinates['y']}
+    while distance_to_move != 0:
+        if current_direction == 1:  # north
+            grid_locations_current_coordinates['y'] += 1
+        elif current_direction == 2:  # east
+            grid_locations_current_coordinates['x'] += 1
+        elif current_direction == 3:  # south
+            grid_locations_current_coordinates['y'] -= 1
+        elif current_direction == 4:  # west
+            grid_locations_current_coordinates['x'] -= 1
 
-        grid_locations[grid_locations_coordinates['x'], grid_locations_coordinates['y']] = 1  # visited
-
-        if bunny_hq_found == 1:
+        if check_if_already_visited(grid_locations_current_coordinates.copy()):
+            bunny_hq_coordinates = grid_locations_current_coordinates.copy()
+            found_hq = True
             return
 
+        grid_locations_coordinates_visited.append(grid_locations_current_coordinates.copy())
+
         # decrement distance left to move
-        instruction_distance -= 1
+        distance_to_move -= 1
 
-    else
-
-
-
-for instruction in directions:
-        instruction_turn = instruction[:1]  # grab turn direction from first character
-        instruction_distance = instruction[1:]  # grab the distance needed to travel from the remaining characters
-
-        change_direction(instruction_turn)  # change direction to whatever direction
-        distance_traveled_in_direction[current_direction] += int(instruction_distance)  # add to the total distance for that direction
-        compute_movement(instruction_distance)
+    return
 
 
-# computing distance
-useful_distance_traveled = abs(distance_traveled_in_direction[1]-distance_traveled_in_direction[3]) + abs(distance_traveled_in_direction[2]-distance_traveled_in_direction[4])
+# actual main functionality
+for instruction in directions.split(', '):
+        change_direction(instruction[:1])  # change direction to whatever direction
+        compute_movement(int(instruction[1:]))
+        if found_hq:
+            break
 
 # solution
-print useful_distance_traveled
+# print grid_locations_coordinates_visited
+print bunny_hq_coordinates
+print abs(bunny_hq_coordinates['x'])+abs(bunny_hq_coordinates['y'])
